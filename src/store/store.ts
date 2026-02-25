f11f211fac9701ca2c7886a4fe8fb58d484596aa
Ermishina Evgeniya <evermishka@yandex.ru>
2026-02-25 18:48:06 +0300

    feat: добавить redux-persist для сохранения состояния
    - Добавлен redux-persist в качестве зависимости в package.json.
    - Настроено сохранение состояния хранилища с использованием localStorage.
    - Создан persistor для управления процессом сохранения состояния.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { contactsReducer, favoritesReducer, groupsReducer, uiReducer } from './reducers';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -11,5 +13,17 @@ const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-// @ts-expect-error - Redux typing issue with newer versions
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const persistedReducer = persistReducer<any, any>(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+export const persistor = persistStore(store);
+
+// Export the root state type
+export type RootStateType = RootState;
